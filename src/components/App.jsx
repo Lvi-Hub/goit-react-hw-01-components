5ea1816e84298fd9d34381701e17549512bbbd7d
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-05-03 20:20:04 +0200

    Task_3 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+// import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import userInfo from 'components/Profile/user.json';
 import data from 'components/Statistics/data.json';
 import friends from 'components/FriendList/friends.json';
-import transactions from 'components/TransactionHistory/transactions.json';
+// import transactions from 'components/TransactionHistory/transactions.json';
 export const App = () => {
   return (
     <div>
